996cf87f25d533baa90d8034c39a8c759a275703
Teona Raluca Artene <teonaartene@yahoo.com>
2024-11-24 11:42:39 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ArteneTeonaRalucaLab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ArteneTeonaRalucaLab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
