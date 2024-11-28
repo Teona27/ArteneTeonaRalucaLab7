997aa8b49c4e4c8eedbd0bfb36f655029c4cd8b6
Teona Raluca Artene <teonaartene@yahoo.com>
2024-11-28 14:40:19 +0200

    Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,31 @@
-﻿namespace ArteneTeonaRalucaLab7
+﻿using System;
+using ArteneTeonaRalucaLab7.Data;
+using System.IO;
+
+namespace ArteneTeonaRalucaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
-
             MainPage = new AppShell();
         }
     }
 }
+
+
